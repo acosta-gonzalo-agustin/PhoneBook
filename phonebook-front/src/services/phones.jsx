6e8734cb3954bf55d3935dfa6ddd16df81957af9
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2026-03-08 19:57:21 -0300

    get errors

--- phonebook-front/src/services/phones.jsx
+++ phonebook-front/src/services/phones.jsx
@@ -6,7 +6,8 @@ const baseUrl = '/api/persons'
 const getAll = () => {
     const request = axios.get(baseUrl)
 
-    return request.then(response => response.data)
+    return request.then(response => response.data).catch(error => console.log(error))
+
 
 }
 
